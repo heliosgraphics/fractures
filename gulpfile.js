618efcb3160b45084a63f53e6784acdee9d3d483
Krisztian <pyx@z8.hu>
2015-12-05 20:32:06 +0100

    Grid file cleanup, fixes gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
+var cssmin = require("gulp-cssmin");
 var gulp = require("gulp");
 var less = require("gulp-less");
-var cssmin = require("gulp-cssmin");
+var rename = require("gulp-rename");
 
-var files = "./src/fractures.less";
+var file = "./src/fractures.less";
 
 gulp.task("default", function() {
-	gulp.src(files)
+	return gulp.src(file)
 		.pipe(less())
 		.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("prod", function () {
-	gulp.src(files)
+gulp.task("prod", function() {
+	return gulp.src(file)
 		.pipe(less())
 		.pipe(cssmin())
+		.pipe(rename("fractures.min.css"))
 		.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("watch", function () {
-	gulp.watch(files, ["default"]);
+gulp.task("watch", function() {
+	return gulp.watch("./src/*.less", ["default"]);
 });
