9451dfb35fd570c4547a9fd35380a0515f3433bf
Krisztian <pyx@z8.hu>
2015-12-12 23:58:03 +0100

    Add gzipped build, fixes #9

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var postcssimport = require("postcss-import");
 var cssnext = require("cssnext");
 var nano = require("gulp-cssnano");
 var rename = require("gulp-rename");
+var gzip = require("gulp-gzip");
+var size = require("gulp-size");
 
 var files = [ "./src/fractures.css" ];
 var postcssProcessors = [
@@ -20,8 +22,14 @@ gulp.task("default", function() {
 gulp.task("build", function() {
 	return gulp.src(files)
 		.pipe(postcss(postcssProcessors))
+		.pipe(size())
 		.pipe(nano())
+		.pipe(size())
 		.pipe(rename("fractures.min.css"))
+		.pipe(gulp.dest("./dist"))
+		.pipe(gzip())
+		.pipe(size())
+		.pipe(rename("fractures.min.css.gz"))
 		.pipe(gulp.dest("./dist"));
 });
 
