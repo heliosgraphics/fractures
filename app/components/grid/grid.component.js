86663796b310004413ccbe50425a6dfc3a1e25cc
Krisztian <pyx@z8.hu>
2016-03-13 14:03:06 +0100

    Grid classes, #20 WIP

--- app/components/grid/grid.component.js
+++ app/components/grid/grid.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "angular2/common"], function(exports_1, context_1) {
+System.register(["angular2/core", "../../shared/pipes/MapToIterable"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,31 +10,169 @@ System.register(["angular2/core", "angular2/common"], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1;
+    var core_1, MapToIterable_1;
     var GridComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (common_1_1) {
-                common_1 = common_1_1;
+            function (MapToIterable_1_1) {
+                MapToIterable_1 = MapToIterable_1_1;
             }],
         execute: function() {
             GridComponent = (function () {
                 function GridComponent() {
-                    this.elementCount = 1;
-                    this.container = {};
-                    this.grid = {};
-                    this.grid.options = {};
-                    this.element = {};
+                    this.elementArray = [{}];
+                    this.elementClasses = [];
+                    this.gridClasses = [];
+                    // TODO Import
+                    this.gridOptions = {
+                        align: {
+                            selected: false,
+                            options: [
+                                "grid--start",
+                                "grid--end",
+                                "grid--baseline"
+                            ]
+                        },
+                        center: {
+                            selected: false,
+                            options: [
+                                "grid--center",
+                                "grid--ycenter",
+                                "grid--xcenter"
+                            ]
+                        },
+                        justify: {
+                            selected: false,
+                            options: [
+                                "grid--around",
+                                "grid--between",
+                                "grid--right"
+                            ]
+                        },
+                        responsive: {
+                            selected: false,
+                            options: [
+                                "grid--collapse",
+                                "grid--screen"
+                            ]
+                        },
+                        reverse: {
+                            selected: false,
+                            options: [
+                                "grid--reverse"
+                            ]
+                        },
+                        wrap: {
+                            selected: false,
+                            options: [
+                                "grid--wrap"
+                            ]
+                        },
+                        column: {
+                            selected: false,
+                            options: [
+                                "grid--column"
+                            ]
+                        }
+                    };
+                    this.elementOptions = {
+                        grow: {
+                            selected: false,
+                            options: [
+                                "grow-1",
+                                "grow-2",
+                                "grow-3",
+                                "grow-4",
+                                "grow-5",
+                            ]
+                        },
+                        order: {
+                            selected: false,
+                            options: [
+                                "order-1",
+                                "order-2",
+                                "order-3",
+                            ]
+                        }
+                    };
+                    this.marginOptions = {
+                        marginPosition: {
+                            selected: false,
+                            options: [
+                                "m",
+                                "mx",
+                                "my",
+                                "mt",
+                                "mb",
+                                "ml",
+                                "mr",
+                            ]
+                        },
+                        marginSize: {
+                            selected: false,
+                            options: [
+                                "0",
+                                "1",
+                                "2",
+                                "3",
+                                "4",
+                            ]
+                        },
+                        paddingPosition: {
+                            selected: false,
+                            options: [
+                                "p",
+                                "px",
+                                "py",
+                                "pt",
+                                "pb",
+                                "pl",
+                                "pr",
+                            ]
+                        },
+                        paddingSize: {
+                            selected: false,
+                            options: [
+                                "0",
+                                "1",
+                                "2",
+                                "3",
+                                "4",
+                            ]
+                        }
+                    };
                 }
+                // Set extra elements length
+                GridComponent.prototype.elementCountUpdate = function (direction) {
+                    if (direction)
+                        return this.elementArray.push({});
+                    else
+                        return this.elementArray.splice(1, 1);
+                };
+                // Control grid classes
+                GridComponent.prototype.toggleGridClass = function (selected, set) {
+                    // Remove all setitem from classes array
+                    for (var _i = 0, _a = this.gridOptions[set].options; _i < _a.length; _i++) {
+                        var option = _a[_i];
+                        var optionIndex = this.gridClasses.indexOf(option);
+                        if (optionIndex !== -1)
+                            this.gridClasses.splice(optionIndex, 1);
+                    }
+                    // Add new class to array
+                    if (selected[1])
+                        this.gridClasses.push(selected[1]);
+                    // Set selected on options
+                    this.gridOptions[set].selected = selected[0];
+                };
                 GridComponent = __decorate([
                     core_1.Component({
+                        pipes: [MapToIterable_1.MapToIterable],
                         selector: "grid",
-                        directives: [common_1.FORM_DIRECTIVES],
-                        templateUrl: "app/components/grid/grid.component.html",
-                        styleUrls: ["app/components/grid/grid.component.css"]
+                        styleUrls: ["app/components/grid/grid.component.css"],
+                        templateUrl: "app/components/grid/grid.component.html"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], GridComponent);
--- app/components/grid/grid.component.ts
+++ app/components/grid/grid.component.ts
@@ -1,26 +1,174 @@
 import { Component } from "angular2/core";
-import { NgClass, NgFor, FORM_DIRECTIVES } from "angular2/common";
+import { NgClass, NgFor } from "angular2/common";
+
+import { MapToIterable } from "../../shared/pipes/MapToIterable";
 
 @Component({
+	pipes: [ MapToIterable ],
 	selector: "grid",
-	directives: [ FORM_DIRECTIVES ],
-	templateUrl: "app/components/grid/grid.component.html",
-	styleUrls: [ "app/components/grid/grid.component.css" ]
+	styleUrls: [ "app/components/grid/grid.component.css" ],
+	templateUrl: "app/components/grid/grid.component.html"
 })
 
 export class GridComponent {
-
-	// WIP
-	public container: any;
-	public elementCount: number;
-	public grid: any;
-	public element: any;
+	public elementArray: any;
+	public elementClasses: any;
+	public elementOptions: any;
+	public gridClasses: any;
+	public gridOptions: any;
+	public marginOptions: any;
 
 	constructor() {
-		this.elementCount = 1;
-		this.container = {};
-		this.grid = {};
-		this.grid.options = {};
-		this.element = {};
+		this.elementArray = [{}]
+		this.elementClasses = [];
+		this.gridClasses = [];
+
+		// TODO Import
+		this.gridOptions = {
+			align: {
+				selected: false,
+				options: [
+					"grid--start",
+					"grid--end",
+					"grid--baseline"
+				]
+			},
+			center: {
+				selected: false,
+				options: [
+					"grid--center",
+					"grid--ycenter",
+					"grid--xcenter"
+				]
+			},
+			justify: {
+				selected: false,
+				options: [
+					"grid--around",
+					"grid--between",
+					"grid--right"
+				]
+			},
+			responsive: {
+				selected: false,
+				options: [
+					"grid--collapse",
+					"grid--screen"
+				]
+			},
+			reverse: {
+				selected: false,
+				options: [
+					"grid--reverse"
+				]
+			},
+			wrap: {
+				selected: false,
+				options: [
+					"grid--wrap"
+				]
+			},
+			column: {
+				selected: false,
+				options: [
+					"grid--column"
+				]
+			}
+		};
+
+		this.elementOptions = {
+			grow: {
+				selected: false,
+				options: [
+					"grow-1",
+					"grow-2",
+					"grow-3",
+					"grow-4",
+					"grow-5",
+				]
+			},
+			order: {
+				selected: false,
+				options: [
+					"order-1",
+					"order-2",
+					"order-3",
+				]
+			}
+		};
+
+		this.marginOptions = {
+			marginPosition: {
+				selected: false,
+				options: [
+					"m",
+					"mx",
+					"my",
+					"mt",
+					"mb",
+					"ml",
+					"mr",
+				]
+			},
+			marginSize: {
+				selected: false,
+				options: [
+					"0",
+					"1",
+					"2",
+					"3",
+					"4",
+				]
+			},
+			paddingPosition: {
+				selected: false,
+				options: [
+					"p",
+					"px",
+					"py",
+					"pt",
+					"pb",
+					"pl",
+					"pr",
+				]
+			},
+			paddingSize: {
+				selected: false,
+				options: [
+					"0",
+					"1",
+					"2",
+					"3",
+					"4",
+				]
+			}
+		}
+	}
+
+	// Set extra elements length
+	elementCountUpdate(direction) {
+		if(direction)
+			return this.elementArray.push({});
+		else
+			return this.elementArray.splice(1, 1);
+	}
+
+	// Control grid classes
+	toggleGridClass(selected, set) {
+
+		// Remove all setitem from classes array
+		for(let option of this.gridOptions[set].options) {
+			let optionIndex = this.gridClasses.indexOf(option);
+
+			if(optionIndex !== -1)
+				this.gridClasses.splice(optionIndex, 1);
+		}
+
+		// Add new class to array
+		if(selected[1])
+			this.gridClasses.push(selected[1]);
+
+		// Set selected on options
+		this.gridOptions[set].selected = selected[0];
 	}
 }
