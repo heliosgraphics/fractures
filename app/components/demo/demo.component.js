9f51ab6948b1f3eebada9e23ef06971a9ec62876
Krisztian <pyx@z8.hu>
2016-03-20 13:14:18 +0100

    Simplified demo component

--- app/components/demo/demo.component.js
+++ app/components/demo/demo.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "./format/format.component", "./grid/grid.component"], function(exports_1, context_1) {
+System.register(["angular2/core", "../../shared/services/options.service", "../../shared/components/options-toggle/options-toggle.component"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,30 +10,64 @@ System.register(["angular2/core", "./format/format.component", "./grid/grid.comp
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, format_component_1, grid_component_1;
+    var core_1, options_service_1, options_toggle_component_1;
     var DemoComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (format_component_1_1) {
-                format_component_1 = format_component_1_1;
+            function (options_service_1_1) {
+                options_service_1 = options_service_1_1;
             },
-            function (grid_component_1_1) {
-                grid_component_1 = grid_component_1_1;
+            function (options_toggle_component_1_1) {
+                options_toggle_component_1 = options_toggle_component_1_1;
             }],
         execute: function() {
             DemoComponent = (function () {
-                function DemoComponent() {
+                function DemoComponent(OptionsService) {
+                    this.OptionsService = OptionsService;
+                    this.optionsStrings = ["box", "grid", "gridelement", "format"];
+                    this.optionsArray = [];
+                    this.boxClasses = [];
+                    this.elementArray = [{}];
+                    this.elementClasses = [];
+                    this.gridClasses = [];
+                    this.formatClasses = ["format"];
+                    this.fontSize = 16;
                 }
+                // TODO: A naïve implementation
+                DemoComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    var _loop_1 = function(x) {
+                        this_1.OptionsService
+                            .getOptions(x)
+                            .subscribe(function (data) {
+                            _this.optionsArray[x] = data;
+                        });
+                    };
+                    var this_1 = this;
+                    for (var _i = 0, _a = this.optionsStrings; _i < _a.length; _i++) {
+                        var x = _a[_i];
+                        _loop_1(x);
+                    }
+                };
+                // Set extra elements length
+                DemoComponent.prototype.elementCountUpdate = function (direction) {
+                    if (direction)
+                        return this.elementArray.push({});
+                    else
+                        return this.elementArray.splice(1, 1);
+                };
                 DemoComponent = __decorate([
                     core_1.Component({
-                        directives: [format_component_1.FormatComponent, grid_component_1.GridComponent],
+                        directives: [options_toggle_component_1.OptionsToggleComponent],
+                        providers: [options_service_1.OptionsService],
                         selector: "demo",
+                        styleUrls: ["app/components/demo/demo.component.css"],
                         templateUrl: "app/components/demo/demo.component.html"
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [options_service_1.OptionsService])
                 ], DemoComponent);
                 return DemoComponent;
             }());
--- app/components/demo/demo.component.ts
+++ app/components/demo/demo.component.ts
@@ -1,13 +1,54 @@
-import { Component } from "angular2/core";
+import { Component, OnInit } from "angular2/core";
 import { NgClass, NgFor, NgIf } from "angular2/common";
 
-import { FormatComponent } from "./format/format.component";
-import { GridComponent } from "./grid/grid.component";
+import { OptionsService } from "../../shared/services/options.service";
+import { OptionsToggleComponent } from "../../shared/components/options-toggle/options-toggle.component";
 
 @Component({
-	directives: [ FormatComponent, GridComponent ],
+	directives: [ OptionsToggleComponent ],
+	providers: [ OptionsService ],
 	selector: "demo",
+	styleUrls: [ "app/components/demo/demo.component.css" ],
 	templateUrl: "app/components/demo/demo.component.html"
 })
 
-export class DemoComponent {}
+export class DemoComponent implements OnInit {
+	public boxClasses: any;
+	public elementArray: any;
+	public elementClasses: any;
+	public fontSize: number;
+	public formatClasses: any;
+	public gridClasses: any;
+
+	public optionsStrings: any = [ "box", "grid", "gridelement", "format" ];
+	public optionsArray: any = [];
+
+	// TODO: A naïve implementation
+	ngOnInit() {
+		for(let x of this.optionsStrings) {
+			this.OptionsService
+				.getOptions(x)
+				.subscribe(data => {
+					this.optionsArray[x] = data;
+				});
+		}
+	}
+
+	constructor(private OptionsService: OptionsService) {
+		this.boxClasses = [];
+		this.elementArray = [{}];
+		this.elementClasses = [];
+		this.gridClasses = [];
+		this.formatClasses = [ "format" ];
+		this.fontSize = 16;
+	}
+
+	// Set extra elements length
+	elementCountUpdate(direction) {
+		if(direction)
+			return this.elementArray.push({});
+		else
+			return this.elementArray.splice(1, 1);
+	}
+
+}
