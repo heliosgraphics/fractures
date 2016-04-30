5087f6fe5840026c7c49b5f6dba07d8e7a988c4f
Krisztian <pyx@z8.hu>
2016-04-30 20:16:49 +0200

    es6 gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
-var gulp = require("gulp");
-var postcss = require("gulp-postcss");
-var postcssimport = require("postcss-import");
-var postcsscssnext = require("postcss-cssnext");
-var nano = require("gulp-cssnano");
-var rename = require("gulp-rename");
-var gzip = require("gulp-gzip");
-var size = require("gulp-size");
-var csslint = require("gulp-csslint");
+"use strict";
 
-var files = [ "./src/fractures.css" ];
-var postcssProcessors = [
+let gulp = require("gulp");
+let postcss = require("gulp-postcss");
+let postcssimport = require("postcss-import");
+let postcsscssnext = require("postcss-cssnext");
+let nano = require("gulp-cssnano");
+let rename = require("gulp-rename");
+let gzip = require("gulp-gzip");
+let size = require("gulp-size");
+let csslint = require("gulp-csslint");
+
+let files = [ "./src/fractures.css" ];
+let postcssProcessors = [
 	postcssimport(),
 	postcsscssnext({ browsers: ["last 2 versions"] })
 ];
 
-gulp.task("default", function() {
+gulp.task("default", () => {
 	return gulp.src(files)
 		.pipe(postcss(postcssProcessors))
 		.pipe(gulp.dest("./dist"))
 		.pipe(size({ showFiles: true }));
 });
 
-gulp.task("build", ["default"], function() {
+gulp.task("build", ["default"], () => {
 	return gulp.src(files)
 		.pipe(postcss(postcssProcessors))
 		.pipe(nano())
@@ -35,12 +37,12 @@ gulp.task("build", ["default"], function() {
 		.pipe(size({ showFiles: true, gzip: true }));
 });
 
-gulp.task("test", ["build"], function() {
+gulp.task("test", ["build"], () => {
 	return gulp.src("./dist/fractures.css")
 		.pipe(csslint("./csslintrc.json"))
 		.pipe(csslint.reporter());
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", () => {
 	return gulp.watch("./src/*.css", ["default"]);
 });
