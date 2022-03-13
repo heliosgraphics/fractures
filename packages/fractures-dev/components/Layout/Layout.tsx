2c5ea697be55d1db66985f814b943ba317f4aa7e
chrispuska <theta@hey.com>
2022-03-13 14:15:43 -0600

    Drop unused babel from fractures-dev

--- packages/fractures-dev/components/Layout/Layout.tsx
+++ packages/fractures-dev/components/Layout/Layout.tsx
@@ -16,18 +16,11 @@ const Layout: React.FC<LayoutProps> = (props) => {
 			<Head>
 				<title>{meta.title}</title>
 				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="initial-scale=1.0, width=device-width"
-				/>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta name="description" content={meta.description} />
 				<meta name="og-url" property="og:url" content={meta.url} />
 				<meta name="og-type" property="og:type" content="website" />
-				<meta
-					name="og-title"
-					property="og:title"
-					content={meta.title}
-				/>
+				<meta name="og-title" property="og:title" content={meta.title} />
 				<meta
 					name="og-description"
 					property="og:description"
@@ -38,26 +31,24 @@ const Layout: React.FC<LayoutProps> = (props) => {
 					property="og:image"
 					content="https://fractures.dev/images/share.png"
 				/>
-				<meta
-					name="og-image-width"
-					property="og:image:width"
-					content="332"
-				/>
-				<meta
-					name="og-image-height"
-					property="og:image:height"
-					content="332"
-				/>
+				<meta name="og-image-width" property="og:image:width" content="332" />
+				<meta name="og-image-height" property="og:image:height" content="332" />
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;700&family=Inter:wght@300;400;500;700;900&family=IBM+Plex+Sans:wght@700&display=swap"
 					rel="stylesheet"
 				/>
-				{/* @ts-expect-error: media not on meta yet */}
-				<meta name="theme-color" content="#220845" media="(prefers-color-scheme: light)"/>
-				{/* @ts-expect-error: media not on meta yet */}
-				<meta name="theme-color" content="#220845" media="(prefers-color-scheme: dark)"/>
+				<meta
+					name="theme-color"
+					content="#220845"
+					media="(prefers-color-scheme: light)"
+				/>
+				<meta
+					name="theme-color"
+					content="#220845"
+					media="(prefers-color-scheme: dark)"
+				/>
 			</Head>
 			<Circles />
 			<Nav />
