3f699a4cd2dff75f8116aff6b5b3b55865c2c5b0
Chris Puska <theta@hey.com>
2022-06-12 00:19:32 -0600

    Fix fractures-dev fuoc

--- packages/fractures-dev/pages/layout.tsx
+++ packages/fractures-dev/pages/layout.tsx
@@ -5,15 +5,15 @@ import P from "../components/P";
 import PageContainer from "../components/PageContainer";
 import SectionBlock from "../components/SectionBlock";
 import SectionGrid from "../components/SectionGrid";
-import {display} from "fractures-library/src/rules/display";
-import {height} from "fractures-library/src/rules/height";
-import {flex} from "fractures-library/src/rules/flex";
-import {grid} from "fractures-library/src/rules/grid";
-import {width} from "fractures-library/src/rules/width";
-import {overflow} from "fractures-library/src/rules/overflow";
-import {position} from "fractures-library/src/rules/position";
-import {margin} from "fractures-library/src/rules/margin";
-import {padding} from "fractures-library/src/rules/padding";
+import { display } from "fractures-library/src/rules/display";
+import { height } from "fractures-library/src/rules/height";
+import { flex } from "fractures-library/src/rules/flex";
+import { grid } from "fractures-library/src/rules/grid";
+import { width } from "fractures-library/src/rules/width";
+import { overflow } from "fractures-library/src/rules/overflow";
+import { position } from "fractures-library/src/rules/position";
+import { margin } from "fractures-library/src/rules/margin";
+import { padding } from "fractures-library/src/rules/padding";
 
 const LayoutPage: React.FC<{}> = () => (
 	<Layout title="Layout">
@@ -21,7 +21,8 @@ const LayoutPage: React.FC<{}> = () => (
 			<H0>Layout</H0>
 			<SectionBlock title="Breakpoints">
 				<P>
-					Fractures has 5 fixed breakpoint prefix to help you build responsive layouts.
+					Fractures has 5 fixed breakpoint prefix to help you build responsive
+					layouts.
 					<br />
 					Resize this page to see how they work.
 				</P>
@@ -30,17 +31,33 @@ const LayoutPage: React.FC<{}> = () => (
 		<BreakPointChart />
 		<PageContainer>
 			<P>
-					Responsive selectors are marked with a purple dot and can be used with any of these prefixes:<br/><br/>
-					<code><b>sm</b>:<em>selector</em></code><br/>
-					<code><b>md</b>:<em>selector</em></code><br/>
-					<code><b>lg</b>:<em>selector</em></code><br/>
-					<code><b>xl</b>:<em>selector</em></code><br/>
-					<code><b>xxl</b>:<em>selector</em></code>
-				</P>
-			<SectionGrid title="Display" items={display}>
-			</SectionGrid>
+				Responsive selectors are marked with a purple dot and can be used with
+				any of these prefixes:
+				<br />
+				<br />
+				<code>
+					<b>sm</b>:<em>selector</em>
+				</code>
+				<br />
+				<code>
+					<b>md</b>:<em>selector</em>
+				</code>
+				<br />
+				<code>
+					<b>lg</b>:<em>selector</em>
+				</code>
+				<br />
+				<code>
+					<b>xl</b>:<em>selector</em>
+				</code>
+				<br />
+				<code>
+					<b>xxl</b>:<em>selector</em>
+				</code>
+			</P>
+			<SectionGrid title="Display" items={display}></SectionGrid>
 			<SectionGrid title="Flex" items={flex} />
-			<SectionGrid title="Grid" items={grid} isExperimental={true}/>
+			<SectionGrid title="Grid" items={grid} isExperimental={true} />
 			<SectionGrid title="Height" items={height} />
 			<SectionGrid title="Width" items={width} />
 			<SectionGrid title="Position" items={position} />
--- packages/fractures-dev/pages/tools.tsx
+++ packages/fractures-dev/pages/tools.tsx
@@ -2,10 +2,10 @@ import Layout from "../components/Layout";
 import SectionGrid from "../components/SectionGrid";
 import H0 from "../components/H0";
 import PageContainer from "../components/PageContainer";
-import {border} from "fractures-library/src/rules/border";
-import {cursor} from "fractures-library/src/rules/cursor";
-import {list} from "fractures-library/src/rules/list";
-import {opacity} from "fractures-library/src/rules/opacity";
+import { border } from "fractures-library/src/rules/border";
+import { cursor } from "fractures-library/src/rules/cursor";
+import { list } from "fractures-library/src/rules/list";
+import { opacity } from "fractures-library/src/rules/opacity";
 
 const Tools: React.FC<{}> = () => (
 	<Layout title="Tools">
