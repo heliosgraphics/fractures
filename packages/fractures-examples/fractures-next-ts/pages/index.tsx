10c8ffff716edd2bcebe862a5482b4c95319233d
Chris Puska <theta@hey.com>
2022-06-11 23:48:35 -0600

    Update test project

--- packages/fractures-examples/fractures-next-ts/pages/index.tsx
+++ packages/fractures-examples/fractures-next-ts/pages/index.tsx
@@ -1,17 +1,25 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Button, Flex, Heading } from "@fractures/ui";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Fractures ✕ NextJS</title>
-        <meta name="description" content="Fractures NextJS Example"/>
-      </Head>
-      <h1>Yellow and Purple</h1>
-      <div className='bg-yellow-500 purple-700 p-4'>Test 500</div>
-      <div className='bg-yellow-300 purple-700 p-4'>Test 300</div>
-      <div className='bg-yellow-100 purple-700 p-4'>Test 100</div>
-    </div>
-  )
+	return (
+		<Flex
+			className={styles.container}
+			isCentered={true}
+			gap={4}
+			padding={16}
+			isColumn={true}
+		>
+			<Head>
+				<title>Fractures ✕ NextJS</title>
+				<meta name="description" content="Fractures NextJS Example" />
+			</Head>
+			<Heading level={1}>Yellow and Purple</Heading>
+			<Button type="primary" value="Hello" color="purple" />
+			<div className="bg-yellow-500 purple-700 p-4">Test 500</div>
+			<div className="bg-yellow-300 purple-700 p-4">Test 300</div>
+			<div className="bg-yellow-100 purple-700 p-4">Test 100</div>
+		</Flex>
+	);
 }
