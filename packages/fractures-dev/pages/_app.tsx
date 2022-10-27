98e108a75ea9984012bc7e1aaabaf57a90e06d55
Chris Puska <theta@hey.com>
2022-10-26 19:02:14 -0600

    Chore: Update analytics

--- packages/fractures-dev/pages/_app.tsx
+++ packages/fractures-dev/pages/_app.tsx
@@ -1,28 +1,10 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as Fathom from "fathom-client";
+import splitbee from "@splitbee/web";
 import "@fractures/css";
 import "./_app.css";
 
+splitbee.init();
+
 const App = ({ Component, pageProps }) => {
-	const router = useRouter();
-
-	useEffect(() => {
-		Fathom.load(`${process.env.FATHOM_FRACTURES_DEV}`, {
-			includedDomains: ["fractures.dev"],
-		});
-
-		function onRouteChangeComplete() {
-			Fathom.trackPageview();
-		}
-
-		router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-		return () => {
-			router.events.off("routeChangeComplete", onRouteChangeComplete);
-		};
-	}, []);
-
 	return <Component {...pageProps} />;
 };
 
