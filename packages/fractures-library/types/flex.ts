e3fc13c225a0cb642098a75e0d25d7d5567c1eba
Chris Puska <theta@hey.com>
2023-03-16 15:33:03 -0600

    Drop non lib props

--- packages/fractures-library/types/flex.ts
+++ packages/fractures-library/types/flex.ts
@@ -2,7 +2,6 @@ import type { FUIScale } from '@fractures/library/types/scale'
 
 export interface FUIFlexProps {
   align?: 'center' | 'left' | 'right';
-  children?: React.ReactNode
   draggable?: boolean
   gap?: FUIScale
   isAround?: boolean
@@ -13,7 +12,6 @@ export interface FUIFlexProps {
   isWrapping?: boolean
   isXCentered?: boolean
   isYCentered?: boolean
-  onClick?: (event: React.MouseEvent) => void
   padding?: FUIScale
   radius?: 1 | 2 | 3;
   yAlign?: 'top' | 'bottom' | 'baseline'
