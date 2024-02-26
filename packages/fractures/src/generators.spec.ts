060712b8a564369bff29167a04d57f04f358b3bf
Chris Puska <theta@hey.com>
2024-02-25 18:31:51 -0800

    Meta: whitespace in test

--- packages/fractures/src/generators.spec.ts
+++ packages/fractures/src/generators.spec.ts
@@ -9,7 +9,8 @@ const MOCK_RULE: FractureRuleType = {
 	},
 }
 
-const MOCK_EXPECTED = `.bg-gray-0 {background-color: hsla(var(--hsl-gray-0), var(--example));}`
+const MOCK_EXPECTED = `.bg-gray-0 {background-color: hsla(var(--hsl-gray-0), var(--example));}
+`
 
 describe("generateRules", () => {
 	it("Generates a sample selector", () => expect(_generateRule(MOCK_RULE)).toEqual(MOCK_EXPECTED))
