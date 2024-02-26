7f0738dc3d862aec2504f6b7b686c59003090c5c
Chris Puska <theta@hey.com>
2024-02-25 17:08:19 -0800

    Export fractures meta

---
+++ packages/fractures/src/rules/index.ts
@@ -0,0 +1,32 @@
+import { borderRadius } from "./border-radius"
+import { cursor } from "./cursor"
+import { display } from "./display"
+import { flex } from "./flex"
+import { grid } from "./grid"
+import { height } from "./height"
+import { list } from "./list"
+import { margin } from "./margin"
+import { opacity } from "./opacity"
+import { overflow } from "./overflow"
+import { padding } from "./padding"
+import { position } from "./position"
+import { type } from "./type"
+import { width } from "./width"
+import type { FractureFiles } from '../types/meta'
+
+export const FRACTURE_FILES: FractureFiles = {
+	borderRadius,
+	cursor,
+	display,
+	flex,
+	grid,
+	height,
+	list,
+	margin,
+	opacity,
+	overflow,
+	padding,
+	position,
+	type,
+	width,
+}
