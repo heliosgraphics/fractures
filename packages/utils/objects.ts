3922bc7d2a375507990de1afeeaf6f130cdafdbf
Chris Puska <theta@hey.com>
2024-04-13 10:37:01 -0700

    Type: Bit more any

--- packages/utils/objects.ts
+++ packages/utils/objects.ts
@@ -1,8 +1,8 @@
-const _orderObjectProperties = (obj: unknown): unknown | object => {
+const _orderObjectProperties = (obj: any): unknown | object => {
 	if (!obj || typeof obj !== 'object' || Array.isArray(obj)) return obj
 
 	return Object.keys(obj).sort().reduce((acc: any, key: any) => {
-		acc[key] = _orderObjectProperties(obj[key])
+		acc[key] = _orderObjectProperties(obj[key] as any)
 
 		return acc
 	}, {})
